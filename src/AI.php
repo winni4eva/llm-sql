409c51a2b9711561a4e12e8c0431476141f18eed
Adam Winnipass <adams4lyf@gmail.com>
2024-07-31 12:16:54 +0000

    Updated ai prompt

--- src/AI.php
+++ src/AI.php
@@ -26,17 +26,17 @@ class AI
         $this->dbConnect();
         $schema = $this->dbInstance->getSchema();
 
-        $question = "Can you generate a only single SQL SELECT query with relational joins if required
-        without any description in the output just a raw SQL command from the statement below \n
-        Get all user accounts \n
-        Using the table schema below.\n
-        $schema";
-
         // $question = "Can you generate a only single SQL SELECT query with relational joins if required
         // without any description in the output just a raw SQL command from the statement below \n
-        // How any transactions have been generated for user accounts \n
+        // Get all user accounts \n
         // Using the table schema below.\n
         // $schema";
+
+        $question = "Can you generate a only single SQL SELECT query with relational joins if required
+        without any description in the output just a raw SQL command from the statement below \n
+        How any transactions have been generated for user accounts \n
+        Using the table schema below.\n
+        $schema";
 
         $promptResponse = (new Llama())->setApiUrl("http://localhost:11434/api/generate")
             ->setModel("llama3")
@@ -48,13 +48,14 @@ class AI
 
 
         if (isset($promptResponse['response'])) {
+            var_dump('Generated Response: ', $promptResponse['response']);
             $sql = (new StringParser())->extractSql($promptResponse['response']);
             $results = $this->dbInstance->query($sql);
             $this->dbInstance->disconnect();
             $formattedResults = json_encode($results);
             
-            $question = "Can you help me generate a report of the List of all accounts by account type (savings or checking) from this query response $formattedResults";
-            //$question = "Can you help me generate a report of the List of all accounts by account type (savings or checking) with their balances and transactions from this query response $formattedResults";
+            //$question = "Can you help me generate a report of the List of all accounts by account type (savings or checking) from this query response $formattedResults";
+            $question = "Can you help me generate a report of the List of all accounts by account type (savings or checking) with their balances and transactions from this query response $formattedResults";
 
             $promptResponse = (new Llama())->setApiUrl("http://localhost:11434/api/generate")
                 ->setModel("llama3")
